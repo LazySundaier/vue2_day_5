5aa0a7326b8092b1e8f0c7a6532642d50022a84e
2795402901@qq.com <2795402901@qq.com>
2024-04-04 22:43:28 +0800

    插槽(作用域插槽)

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,46 @@
 <template>
   <div>
-    <MyDialog>
-      <!-- v-slot可以简写为# -->
-      <template #head>大标题</template>
-      <template #content>内容区</template>
-      <template #footer>
-        <button>取消</button>
-        <button>确认</button>
+    <MyTable :data="list">
+      <template #default="obj">
+        <!-- {{ obj }} -->
+        <button @click="read(obj.ITEM)">查看</button>
       </template>
-    </MyDialog>
+    </MyTable>
+    <MyTable :data="list2">
+      <template #default="obj">
+        <button @click="del(obj.ITEM.id)">删除</button>
+      </template>
+    </MyTable>
   </div>
 </template>
 
 <script>
-import MyDialog from './components/MyDialog.vue'
+import MyTable from './components/MyTable.vue'
 export default {
-  data () {
+  data() {
     return {
-
+      list: [
+        { id: 1, name: '张小花', age: 18 },
+        { id: 2, name: '孙大明', age: 19 },
+        { id: 3, name: '刘德忠', age: 17 },
+      ],
+      list2: [
+        { id: 1, name: '赵小云', age: 18 },
+        { id: 2, name: '刘蓓蓓', age: 19 },
+        { id: 3, name: '姜肖泰', age: 17 },
+      ]
     }
   },
   components: {
-    MyDialog
+    MyTable
+  },
+  methods: {
+    del(id) {
+      this.list2 = this.list2.filter((item) => item.id !== id)
+    },
+    read(item) {
+      alert(`姓名:${item.name};年纪:${item.age}`)
+    }
   }
 }
 </script>
-
-<style>
-body {
-  background-color: #b3b3b3;
-}
-</style>
